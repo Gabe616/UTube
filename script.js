6651ad476c6ccb8f7a95b48b30e1f1226541d781
WurstMod <wurstmodcorp@gmail.com>
2022-06-16 16:01:36 +0200

    added new stufF!! 	modified:   script.js

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ let colors = {
   cyan: "#2ff",
 };
 let colr = colors.green;
+let txtnd = document.createTextNode("slay");
+let adblock = true;
 let makeCss = () => {
-  return `
+  txtnd.textContent = `
     .ytp-swatch-background-color {
         background-color: ${colr} !important;
     }
@@ -19,39 +21,150 @@ let makeCss = () => {
         color: ${colr} !important;
     }
     html {
-        --yt-spec-brand-button-background: ${colr}
+        --yt-spec-brand-button-background: ${colr} !important
     }
     
-    .adblk {
-        display: none;
-        opacity: 0;
+    ${
+      adblock
+        ? `.adblk {
+      display: none !important;
+      opacity: 0 !important;
+      visibility: hidden !important;
+    }`
+        : ""
     }`;
 };
 
+if (window.ytstyle) window.ytstyle.remove();
+
+makeCss();
+
 let stylesheet = document.createElement("style");
-stylesheet.innerHTML = makeCss();
+stylesheet.type = "text/css";
+stylesheet.appendChild(txtnd);
+window.ytstyle = stylesheet;
 document.head.appendChild(stylesheet);
 
 window.ytbar = {};
 for (let [k, b] of Object.entries(colors)) {
   window.ytbar[k] = () => {
     colr = b;
-    stylesheet.innerHTML = makeCss();
+    makeCss();
   };
 }
+window.ytbar.custom = (str) => {
+  colr = str;
+  makeCss();
+};
+
+let avdd;
+let isloser = false;
+
+let typeFunc = (e) => {
+  let ddt = e.split(/ +/g);
+  let [cmd, ignorethislmao, ...args] = ddt;
+
+  switch (cmd) {
+    case "ytbar":
+      if (!args[0]) return;
+
+      let CLR = colors[args[0]];
+      let nclr;
+
+      if (args[0] === "custom") {
+        nclr = args[1];
+      } else if (CLR) {
+        nclr = CLR;
+      }
+
+      if (nclr && colr !== nclr) {
+        colr = nclr;
+        makeCss();
+      }
+    case "adblock":
+      if (!args[0]) return;
+
+      let yes = "yes sure true zapni ano jj klidně díky slay".split(" ");
+      let no = "no nah false vypni ne nn nikdy bruh unslay deslay".split(" ");
+
+      let y1 = yes.includes(args[0].toLowerCase());
+      let y2 = no.includes(args[0].toLowerCase());
+
+      if (y1 || y2) {
+        blk = y1 ? true : false;
+
+        if (adblock !== blk) {
+          adblock = blk;
+          makeCss();
+        }
+      }
+      break;
+  }
+};
 
 if (window.ytbarTimer) clearInterval(window.ytbarTimer);
 window.ytbarTimer = setInterval(() => {
   let vid = document.querySelector("video[src]");
   let adtxt = document.querySelector("div.ytp-ad-player-overlay");
-  let A = document.querySelector("#player-ads");
+  let yticn = document.querySelectorAll("yt-icon#logo-icon");
+  let vspinner = document.querySelector("div.ytp-spinner");
+  let vchrome = document.querySelector("div.ytp-chrome-bottom");
+  let signin = document.querySelector(
+    "ytd-masthead ytd-button-renderer a[href]"
+  );
+  let sform = document.querySelector("#search-form input#search");
+  let ads = [];
+  let adKeys = [
+    "#player-ads",
+    "ytd-banner-promo-renderer",
+    "masthead-ad",
+    "div.video-ads.ytp-ad-module",
+  ];
+  for (let x of adKeys) {
+    for (let y of document.querySelectorAll(x)) {
+      ads.push(y);
+    }
+  }
 
-  if (A && !A.classList.contains("adblk")) {
-    A.classList.add("adblk");
+  for (let a of ads) {
+    if (!a.classList.contains("adblk")) {
+      a.classList.add("adblk");
+    }
   }
 
   if (vid && adtxt) {
     vid.currentTime = vid.duration;
+    vid.volume = 0;
+    isloser = true;
+
+    if (vspinner) vspinner.style.display = "block";
+    if (vchrome) vchrome.style.display = "none";
+    if (vid) vid.style.display = "none";
+  } else if (vid) {
+    if (isloser) {
+      if (vspinner) vspinner.style.display = "none";
+      if (vchrome) vchrome.style.display = "";
+      if (vid) vid.style.display = "";
+
+      isloser = false;
+      vid.volume = avdd;
+    } else {
+      avdd = vid.volume;
+    }
+  }
+
+  if (sform) {
+    typeFunc(sform.value);
+  }
+
+  for (let x of yticn) {
+    let l = x.querySelector("path");
+    l.attributes.fill.textContent = colr;
+  }
+
+  if (signin) signin = signin.parentNode;
+  if (signin && !signin.classList.contains("adblk")) {
+    signin.classList.add("adblk");
   }
 
   let g = [];
